026fceacf5bd8cd3f96bac07f918314ea7bc54b5
saminafisdev <nafis@joinventureai.com>
2026-03-28 16:37:04 +0600

    feat: add models to the admin panel

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Course)
+admin.site.register(Category)
+admin.site.register(Scholarship)
+admin.site.register(Module)
+admin.site.register(Lesson)
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Option)
+admin.site.register(Assignment)
