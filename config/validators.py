eafb6714c266ab4f004af9cca98335ec94dc9973
saminafisdev <nafis@joinventureai.com>
2026-05-17 11:25:54 +0600

    Expand HTML sanitization for blog content: allow most safe tags, attributes, and styles for admin/teacher input while blocking dangerous elements. Keeps rich formatting but protects against XSS.

--- config/validators.py
+++ config/validators.py
@@ -1,11 +1,29 @@
 # config/validators.py
 import bleach
 
-ALLOWED_TAGS = [
-    'p', 'strong', 'em', 'u', 'h1', 'h2', 'h3',
-    'ul', 'ol', 'li', 'a', 'blockquote', 'code', 'pre'
+# Recommended: allow most safe tags/attrs for admin blog content
+ALLOWED_TAGS = bleach.sanitizer.ALLOWED_TAGS + [
+    'p', 'span', 'div', 'img', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+    'table', 'thead', 'tbody', 'tfoot', 'tr', 'th', 'td', 'pre', 'code',
+    'blockquote', 'ul', 'ol', 'li', 'br', 'hr', 'sup', 'sub', 'u', 's', 'mark', 'small', 'b', 'i', 'strong', 'em'
 ]
-ALLOWED_ATTRIBUTES = {'a': ['href', 'title', 'target']}
-
+ALLOWED_ATTRIBUTES = {
+    '*': ['style', 'class', 'id', 'title'],
+    'a': ['href', 'title', 'target', 'rel'],
+    'img': ['src', 'alt', 'title', 'width', 'height', 'style', 'class'],
+    'td': ['colspan', 'rowspan', 'style', 'class'],
+    'th': ['colspan', 'rowspan', 'style', 'class'],
+}
+ALLOWED_STYLES = [
+    'color', 'background-color', 'font-weight', 'font-style', 'text-decoration', 'font-size',
+    'width', 'height', 'border', 'border-collapse', 'padding', 'margin', 'text-align',
+    'vertical-align', 'float', 'display', 'max-width', 'min-width', 'max-height', 'min-height',
+]
 def sanitize_html(value):
-    return bleach.clean(value, tags=ALLOWED_TAGS, attributes=ALLOWED_ATTRIBUTES)
+    return bleach.clean(
+        value,
+        tags=ALLOWED_TAGS,
+        attributes=ALLOWED_ATTRIBUTES,
+        styles=ALLOWED_STYLES,
+        strip=True
+    )
